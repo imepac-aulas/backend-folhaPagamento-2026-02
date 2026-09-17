eb8c7a264534cd512f3ddab466623ad69ef656ad
FreitasEverton <everton.freitas@imepac.edu.br>
2026-09-17 20:44:12 -0300

    Entidade professor

---
+++ src/main/java/br/edu/imepac/folhapagamento/entities/Professor.java
@@ -0,0 +1,33 @@
+package br.edu.imepac.folhapagamento.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "professores")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome não pode ser nulo")
+    private String nome;
+
+    private String email;
+
+    @Column(name = "quantidade_aulas_mes")
+    private int quantidadeAulasMes;
+
+    @Column(name = "valor_hora")
+    private float valorHora;
+
+    public double calcularSalario() {
+        return quantidadeAulasMes * valorHora;
+    }
+}
