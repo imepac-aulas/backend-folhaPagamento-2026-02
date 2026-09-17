7a7a4ffb47485dba4993ce53fafabded50772347
FreitasEverton <everton.freitas@imepac.edu.br>
2026-09-17 19:28:02 -0300

    Initial commit

---
+++ src/test/java/br/edu/imepac/folhapagamento/FolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.imepac.folhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
