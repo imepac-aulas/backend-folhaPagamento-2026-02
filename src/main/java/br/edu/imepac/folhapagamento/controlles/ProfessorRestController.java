d4c1243d9c0ebca50d8a9226a8c29b96fcac94ca
alefcristiano367 <2526510309@aluno.imepac.edu.br>
2026-09-23 20:02:12 -0300

    feat: adiciona controller service repository e docker

---
+++ src/main/java/br/edu/imepac/folhapagamento/controlles/ProfessorRestController.java
@@ -0,0 +1,24 @@
+package br.edu.imepac.folhapagamento.controlles;
+
+import br.edu.imepac.folhapagamento.entities.Professor;
+import br.edu.imepac.folhapagamento.repositories.ProfessorRepository;
+import br.edu.imepac.folhapagamento.services.ProfessorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping( "/api/professores")
+
+public class ProfessorRestController {
+    private ProfessorService professorService;
+
+    public ProfessorRestController(ProfessorService professorService) {
+        this.professorService = professorService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Professor save(@RequestBody Professor professor){
+        return professorService.save(professor);
+    }
+}
---
+++ src/main/java/br/edu/imepac/folhapagamento/services/ProfessorService.java
@@ -0,0 +1,19 @@
+package br.edu.imepac.folhapagamento.services;
+
+import br.edu.imepac.folhapagamento.entities.Professor;
+import br.edu.imepac.folhapagamento.repositories.ProfessorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService {
+
+    private ProfessorRepository professorRepository;
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    public Professor save (Professor professor){
+        return professorRepository.save(professor);
+    }
+}
